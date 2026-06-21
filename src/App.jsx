f9610b43f3b38aea5c89353c2491103fcc758b4e
Edward Shanahan <edwardshanahan737@gmail.com>
2026-06-21 16:12:45 +0100

    Add base CSS styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ const App = () => {
   return (
     <div>
       <h1>Hello World!</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro qui
+        corrupti similique laudantium molestiae est adipisci provident,
+        voluptate, quis, ipsam labore explicabo praesentium suscipit ratione
+        nesciunt! At eligendi dolor nam.
+      </p>
     </div>
   );
 };
