f9be43b2d038662ec692aad3c1e2d2883d258350
Edward Shanahan <edwardshanahan737@gmail.com>
2026-06-27 17:17:19 +0100

    Add images to Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,3 +1,6 @@
+import heroImgSmall from "../../assets/images/hero-small.webp";
+import heroImgLarge from "../../assets/images/hero-large.webp";
+import heroPatternImg from "../../assets/images/hero-pattern.svg";
 import Button from "../Button/Button";
 
 const Hero = () => {
@@ -11,6 +14,21 @@ const Hero = () => {
       </p>
 
       <Button text="Start exploring" size="large" className="hero__cta" />
+
+      <picture className="hero__img">
+        <source src={heroImgLarge} media="(min-width: 768px)" />
+        <img
+          className="hero__img"
+          src={heroImgSmall}
+          alt="Person cooking a healthy meal"
+        />
+      </picture>
+
+      <img
+        className="hero__pattern"
+        src={heroPatternImg}
+        alt="Pattern Illustration"
+      />
     </div>
   );
 };
