07001abc0459c9cdf80d24eef444bd142fc8261d
Edward Shanahan <edwardshanahan737@gmail.com>
2026-07-15 14:01:41 +0100

    Add RecipeFilters component

---
+++ src/pages/Recipes/RecipeFilters/RecipeFilters.jsx
@@ -0,0 +1,31 @@
+import SearchIcon from "../../../assets/icons/icon-search.svg";
+
+const RecipeFilters = () => {
+  return (
+    <section className="filters">
+      <select name="prep-time" id="prep-time">
+        <option value="">Max Prep Time</option>
+        <option value="0">0 Minutes</option>
+        <option value="5">5 Minutes</option>
+        <option value="10">10 Minutes</option>
+      </select>
+
+      <select name="cook-time" id="cook-time">
+        <option value="">Max Cook Time</option>
+        <option value="0">0 Minutes</option>
+        <option value="5">5 Minutes</option>
+        <option value="10">10 Minutes</option>
+        <option value="15">15 Minutes</option>
+        <option value="20">20 Minutes</option>
+      </select>
+
+      <div className="filters__search">
+        <img src={SearchIcon} alt="Search Icon" />
+
+        <input placeholder="Search by name or ingredient…" type="text" />
+      </div>
+    </section>
+  );
+};
+
+export default RecipeFilters;
--- src/pages/Recipes/Recipes.jsx
+++ src/pages/Recipes/Recipes.jsx
@@ -1,9 +1,12 @@
 import Intro from "./Intro/Intro";
+import RecipeFilters from "./RecipeFilters/RecipeFilters";
 
 const Recipes = () => {
   return (
     <>
       <Intro />
+
+      <RecipeFilters />
     </>
   );
 };
