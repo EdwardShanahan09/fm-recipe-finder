e6a1ad9cbc8217ec28344c7bad724a04c87c0067
Edward Shanahan <edwardshanahan737@gmail.com>
2026-07-23 18:53:14 +0100

    Style the recipe filters

--- src/pages/Recipes/RecipeFilters/RecipeFilters.jsx
+++ src/pages/Recipes/RecipeFilters/RecipeFilters.jsx
@@ -1,5 +1,7 @@
 import SearchIcon from "../../../assets/icons/icon-search.svg";
+import ChevronIcon from "../../../assets/icons/icon-chevron.svg";
 import "./RecipeFilters.css";
+
 const RecipeFilters = ({
   search,
   setSearch,
@@ -10,38 +12,47 @@ const RecipeFilters = ({
 }) => {
   return (
     <section className="filters">
-      <select
-        value={maxPrepTime}
-        onChange={(e) => setMaxPrepTime(e.target.value)}
-        name="prep-time"
-        id="prep-time"
-      >
-        <option value="">Max Prep Time</option>
-        <option value="0">0 Minutes</option>
-        <option value="5">5 Minutes</option>
-        <option value="10">10 Minutes</option>
-      </select>
+      <div className="filters__select-wrap">
+        <select
+          className="filters__select"
+          value={maxPrepTime}
+          onChange={(e) => setMaxPrepTime(e.target.value)}
+          name="prep-time"
+          id="prep-time"
+        >
+          <option value="">Max Prep Time</option>
+          <option value="0">0 Minutes</option>
+          <option value="5">5 Minutes</option>
+          <option value="10">10 Minutes</option>
+        </select>
+        <img className="filters__select-icon" src={ChevronIcon} alt="" />
+      </div>
 
-      <select
-        value={maxCookTime}
-        onChange={(e) => {
-          setMaxCookTime(e.target.value);
-        }}
-        name="cook-time"
-        id="cook-time"
-      >
-        <option value="">Max Cook Time</option>
-        <option value="0">0 Minutes</option>
-        <option value="5">5 Minutes</option>
-        <option value="10">10 Minutes</option>
-        <option value="15">15 Minutes</option>
-        <option value="20">20 Minutes</option>
-      </select>
+      <div className="filters__select-wrap">
+        <select
+          className="filters__select"
+          value={maxCookTime}
+          onChange={(e) => {
+            setMaxCookTime(e.target.value);
+          }}
+          name="cook-time"
+          id="cook-time"
+        >
+          <option value="">Max Cook Time</option>
+          <option value="0">0 Minutes</option>
+          <option value="5">5 Minutes</option>
+          <option value="10">10 Minutes</option>
+          <option value="15">15 Minutes</option>
+          <option value="20">20 Minutes</option>
+        </select>
+        <img className="filters__select-icon" src={ChevronIcon} alt="" />
+      </div>
 
       <div className="filters__search">
         <img src={SearchIcon} alt="Search Icon" />
 
         <input
+          className="filters__search-input"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           placeholder="Search by name or ingredient…"
